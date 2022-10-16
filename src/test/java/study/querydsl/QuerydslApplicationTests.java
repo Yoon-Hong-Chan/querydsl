0bb3b901f07279eadc8c5aa4076335a444293cc8
Yoon-Hong-Chan <ychqwertyuio@gmail.com>
2022-10-17 01:21:19 +0900

    feat : Member 및 Team 세팅

--- src/test/java/study/querydsl/QuerydslApplicationTests.java
+++ src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -1,37 +1,15 @@
 package study.querydsl;
 
-import static org.assertj.core.api.Assertions.*;
-
-import com.querydsl.jpa.impl.JPAQueryFactory;
-import javax.persistence.EntityManager;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
-import study.querydsl.entity.Hello;
-import study.querydsl.entity.QHello;
 
 @SpringBootTest
 @Transactional
 class QuerydslApplicationTests {
 
-	@Autowired
-	EntityManager em;
-
 	@Test
 	void contextLoads() {
-		Hello hello = new Hello();
-		em.persist(hello);
-
-		JPAQueryFactory queryFactory = new JPAQueryFactory(em);
-		QHello qHello = new QHello("h");
-
-		Hello result = queryFactory.selectFrom(qHello)
-				.fetchOne();
-
-		assertThat(result).isEqualTo(hello);
 	}
 
 }
